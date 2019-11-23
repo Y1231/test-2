86f073d20c513cd50e2ce34b1e11c094af25fbb2
komorebi1231 <komorebi_1231@qq.com>
2019-11-23 21:20:10 +0800

    lx1123

---
+++ lx1123.java
@@ -0,0 +1,66 @@
+// public class lx1123{
+//     public static void main (String arge[]){
+//         int [] arr={1,2,3};
+//         transform(arr);
+//         printArray(arr);
+//     }
+//     public static void printArray(int [] arr){
+//         for(int i=0;i<=arr.length;i++){
+//             System.out.println(arr [i]);
+//         }
+//     }
+//     public static void transform(int [] arr){
+//         for(int i=0;i<=arr.length;i++){
+//             arr[i]=arr[i]*2;
+//         }
+//     }
+//     }
+
+
+// public class lx1123{
+//     public static void main (String arge[]){
+//         int [] arr={1,2,3};
+//         int [] output=transform (arr);
+//         printArray(output);
+//     }
+//     public static void printArray(int [] arr){
+//         for(int i=0;i<=arr.length;i++);
+//         System.out.println(arr [i]);
+//     }
+// }
+//     public static void transform(int [] arr){
+//         int [] ret=new int[arr.length];
+//         for(int i=0;i<=arr.length;i++){
+//             ret[i]=arr[i]*2;
+//     }
+//         return ret;
+// }
+
+
+// public class lx1123{
+//     public static void main (String arge[]){
+// import java.util.Arrays;
+// int []arr={1,2,3,4,5,6};
+// String newArr=Arrays.toString(arr);
+// System.out.println(newArr);
+//     }
+// }
+
+
+// public class lx1123{
+//     public static void main (String arge[]){
+//         int []arr={1,2,3,4,5,6};
+//         System.out.println(toString(arr));
+//     }
+// public static String toString(int []arr){
+//     String ret="[";
+//     for(int i=0;i<=arr.length;i++){
+//         ret+=arr[i];
+//         if(i!=arr.length-1){
+//             ret+=",";
+//         }
+//     }
+//     ret+="]";
+//     return ret;
+// }
+// }
